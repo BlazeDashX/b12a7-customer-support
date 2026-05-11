4198173d3fc414e2587ab414fc409e1dada7dac3
BlazeDashX <refat00021@gmail.com>
2026-05-11 23:11:23 +0600

    added hero stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import Navbar from './components/Navbar'
-import './App.css'
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Hero from "./components/Hero";
 
 function App() {
-
   return (
     <>
-    <Navbar></Navbar>
+      <Navbar></Navbar>
+      <div className="max-w-300 mx-auto mt-4 ">
+        <Hero></Hero>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import leftImg from "../assets/vector1.png";
+
+const Hero = () => {
+  return (
+    <div className="flex gap-5 pt-15">
+      <div className="relative h-64 flex-1 overflow-hidden rounded-xl bg-linear-to-r from-[#7038E0] to-[#9F5CF0]">
+
+        <img
+          src={leftImg}
+          alt=""
+          className="absolute left-0 top-0 w-3/7 h-full "
+        />
+
+        <img
+          src={leftImg}
+          alt=""
+          className="absolute right-0 bottom-0 w-3/7 h-full scale-x-[-1]"
+        />
+
+        <div className="relative z-10 flex h-full flex-col items-center justify-center ">
+          <h1 className="text-2xl font-bold text-white">
+            In-Progress
+          </h1>
+
+          <span className="text-6xl font-bold text-white">
+            0
+          </span>
+        </div>
+      </div>
+
+        <div className="relative h-64 flex-1 overflow-hidden rounded-xl bg-linear-to-r from-[#56D067] to-[#0D8372]">
+
+        <img
+          src={leftImg}
+          alt=""
+          className="absolute left-0 top-0 w-3/7 h-full "
+        />
+
+        <img
+          src={leftImg}
+          alt=""
+          className="absolute right-0 bottom-0 w-3/7 h-full scale-x-[-1]"
+        />
+
+        <div className="relative z-10 flex h-full flex-col items-center justify-center ">
+          <h1 className="text-2xl font-bold text-white">
+            Resolved
+          </h1>
+
+          <span className="text-6xl font-bold text-white">
+            0
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
